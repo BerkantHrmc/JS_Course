9a3d0da6313eab64ab03619a448af204ea48a9af
BerkantHRMC <berkanthurmaci2@gmail.com>
2025-07-29 23:23:33 +0300

    exp

--- 16. High Order Functins - Yüksek D. Fonk/app.js
+++ 16. High Order Functins - Yüksek D. Fonk/app.js
@@ -1,14 +1,30 @@
-function sayHello() {
-  console.log('Hello')
-}
-setInterval(sayHello, 1000) 
+const countries = ['Finland', 'Sweden', 'Denmark', 'Norway', 'IceLand']
+const names = ['Asabeneh', 'Mathias', 'Elias', 'Brook']
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+const products = [
+  { product: 'banana', price: 3 },
+  { product: 'mango', price: 6 },
+  { product: 'potato', price: ' ' },
+  { product: 'avocado', price: 8 },
+  { product: 'coffee', price: 10 },
+  { product: 'tea', price: '' },
+]
 
-let sum = 0;
-const numbers = [1, 2, 3, 4, 5];
-numbers.forEach(num => console.log(num))
+
+// - Use map to create a new array by changing each country to uppercase in the countries array.
+
+const upCas = countries.map((countrie) => countrie.toUpperCase());
+console.log(upCas);
+
+
+//- Use filter to filter out countries containing land.
+
+const landCount = countries.filter((countrie) => countrie.endsWith("land"));
+console.log(landCount);
+
+//- Use reduce to sum all the numbers in the numbers array.
+
+const sum = numbers.reduce((toplam,sayi) => toplam + sayi , 0);
 console.log(sum);
 
-
-const numbersSquare = numbers.map((num) => num * num)
-
-console.log(numbersSquare)
+//-
